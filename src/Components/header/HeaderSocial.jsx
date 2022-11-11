1528b93f55a18dc16b9966696edbabc14a739618
Fei C <alexbpontello@gmail.com>
2022-11-11 15:08:14 -0500

    c

--- src/Components/header/HeaderSocial.jsx
+++ src/Components/header/HeaderSocial.jsx
@@ -6,13 +6,13 @@ import { BsFacebook } from "react-icons/bs";
 const HeaderSocial = () => {
   return (
     <div className="header_socials">
-      <a href="https://www.linkedin.com/in/feifan-cao/" target="_blank">
+      <a href="https://www.linkedin.com/in/feifan-cao/" target="blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com/FeifanC" target="_blank">
+      <a href="https://github.com/FeifanC" target="blank">
         <BsGithub />
       </a>
-      <a href="https://www.facebook.com/alexander.pontello/" target="_blank">
+      <a href="https://www.facebook.com/alexander.pontello/" target="blank">
         <BsFacebook />
       </a>
     </div>
